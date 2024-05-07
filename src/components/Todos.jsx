d5429754165d4af1b77bdc851b7f2e972aea64fc
Shashank Naik <shashanknaik808@gmail.com>
2024-05-07 21:31:10 +0530

    Styled todo list items and delete buttons

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -13,9 +13,17 @@ function Todos() {
             <div>Todos</div>
             <ul className="list-none">
                 {todos.map((todo) => (
-                    <li key={todo.id}>
-                        {todo.text}
-                        <button onClick={() => dispatch(removeTodo(todo.id))}>Delete</button>
+                    <li
+                        className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+                        key={todo.id}
+                    >
+                        <div className='text-white'>{todo.text}</div>
+                        <button
+                            onClick={() => dispatch(removeTodo(todo.id))}
+                            className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
+                        >
+                            Delete
+                        </button>
                     </li>
                 ))}
             </ul>
