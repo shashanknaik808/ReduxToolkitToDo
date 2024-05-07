2dc902e11e2f85a1cc2fce9fc15d5c395ca1d1bb
Shashank Naik <shashanknaik808@gmail.com>
2024-05-07 21:58:49 +0530

    Revised action names in todoSlice to follow camelCase convention

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,27 +1,28 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 const initialState = {
-    todos: [{ id: 1, text: "Hello World" }]
+    todos: [{ id: 1, text: "Hello world" }]
 }
+
+
 
 export const todoSlice = createSlice({
     name: 'todo',
     initialState,
     reducers: {
-        addToDo: (state, action) => {
+        addTodo: (state, action) => {
             const todo = {
                 id: nanoid(),
                 text: action.payload
             }
             state.todos.push(todo)
         },
-        removeToDo: (state, action) => {
-            state.todos = state.todos.filter((todo) => todo.
-                id !== action.payload)
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
         },
     }
 })
 
-export const {addToDo, removeToDo} = todoSlice.actions
+export const { addTodo, removeTodo } = todoSlice.actions
 
 export default todoSlice.reducer
