a456116215b8a400fa0776da25f7ed763556f1c8
Shashank Naik <shashanknaik808@gmail.com>
2024-05-07 21:26:45 +0530

    Used useSelector to select todos from state and useDispatch to get dispatch function

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -4,6 +4,10 @@ import {removeTodo} from '../features/todo/todoSlice'
 
 
 function Todos() {
+
+    const todos = useSelector(state => state.todos)
+    const dispatch = useDispatch()
+
     return (
         <div>Todos</div>
     )
