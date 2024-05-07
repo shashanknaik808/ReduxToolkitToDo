2cb3c8445c5ad1ce2bcf044faa4f5e37440d990a
Shashank Naik <shashanknaik808@gmail.com>
2024-05-07 21:18:58 +0530

    Implemented addTodoHandler function to add todo item

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 
 function AddTodo() {
+
+    const [input, setInput] = useState('')
+    const dispatch = useDispatch()
+
+    const addTodoHandler = (e) => {
+        e.preventDefault()
+        dispatch(addTodo(input))
+        setInput('')
+    }
+
     return (
         <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
             <input
